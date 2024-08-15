b6640ec0e417bf69465d1e1707c4e150113a43a7
Alikhan Serikbolov <alserikbolov@gmail.com>
2024-08-15 21:29:40 +0600

    Updating Registration page

---
+++ accounts/validators.py
@@ -0,0 +1,34 @@
+import re
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+class CustomPasswordValidator:
+    def validate(self, password, user=None):
+        if len(password) < 8 or len(password) > 15: # password length
+            raise ValidationError(
+                "The password must be between 8 and 15 characters long.",
+            code='password_length',
+            )
+        if not re.search(r'[A-Z]', password): # password upper letter
+            raise ValidationError(
+                _("The password must contain at least one uppercase letter."),
+            code="password_no_apper",
+            )
+        if not re.search(r'[a-z]', password): # password lower letter
+            raise ValidationError(
+                _("The password must contain at least one lower letter."),
+            code="password_no_lower",
+            )
+        if not re.search(r'\d', password): # password contain number
+            raise ValidationError(
+                _("The password must contain at least one number."),
+            code="password_no_number"
+            )
+        if not re.search(r'[!@#$%^&*()<>?,./:;{}|]', password):
+            raise ValidationError(
+                _("The password must contain at least one special character.", password),
+            code="password_no_special",
+            )
+        
+    def get_help_text(self):
+        return _("The password must be between 8 and 15 characters long and include at least one lowercase and one uppercase letter, one number, and one special character (!, @, #, $, ...).")
--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import CreateUserForm
+
 
 def register(request):
     if request.method != 'POST':
-        form = UserCreationForm()
+        form = CreateUserForm()
     else:
-        form = UserCreationForm(data=request.POST)
-
+        form = CreateUserForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
             login(request, new_user)
