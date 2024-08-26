6a7e3230e8d817974f485d065e4fba057237fd56
Alikhan Serikbolov <alserikbolov@gmail.com>
2024-08-26 13:36:10 +0600

    Implemented Email confirmation

--- accounts/models.py
+++ accounts/models.py
@@ -3,14 +3,21 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from django.db import models
-import uuid
+import random
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    confirmation_code = models.UUIDField(default=uuid.uuid4, editable=False)
+    confirmation_code = models.CharField(max_length=4, blank=True, unique=True)
     is_confirmed = models.BooleanField(default=False)
 
+    def save(self, *args, **kwargs):
+        if not self.confirmation_code:
+            self.confirmation_code = self.generate_confiramtion_code()
+        super(Profile, self).save(*args, **kwargs)
+
+    def generate_confiramtion_code(self):
+        return str(random.randint(1000, 9999))
     
     def __str__(self) -> str:
         return self.user.username
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 from .forms import CreateUserForm
-from django.core.mail import send_mail
+from django.core.mail import send_mail, BadHeaderError
 from .models import Profile
 
 
@@ -13,28 +13,41 @@ def register(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            profile, created = Profile.objects.get_or_create(user=user)
+            user = form.save() # user is saved in DB
+            
+            try:
+                profile, created = Profile.objects.get_or_create(user=user) # user Profile created
 
-            # Sending message with the code
-            send_mail(
-                'Registration confirmation',
-                f'Your confirmation code: {profile.confirmation_code}',
-                'alserikbolov@mail.ru',
-                [user.email],
-                fail_silently=False,
-            )
-            return redirect('accounts:confirm_email')
+                send_mail( # Sending message with the code
+                    "Hi! It's Alikhan.",
+                    f'Here is your confirmation code: {profile.confirmation_code}',
+                    'alserikbolov@mail.ru',
+                    [user.email],
+                    fail_silently=False,
+                )
+
+                return redirect('accounts:confirm_email')
+            
+            # Email error message
+            except BadHeaderError:
+                user.delete() # deleting user from DB if error appears
+                error_message = "Email is not exist. Please check it again."
+            except Exception as e:
+                user.delete()
+                error_message = f"This email is not exist. Please write another one."
+                
+            return render(request, 'registration/register.html', {'form':form, 'error': error_message})
+            
     else:
         form = CreateUserForm()
         
-    context = {'form':form}
-    return render(request, 'registration/register.html', context)
+    
+    return render(request, 'registration/register.html', {'form':form})
 
 
-
-# Processing verification code
-"""The code from the form is checked, if it exists and matches, 
+"""
+Processing verification code
+The code from the form is checked, if it exists and matches, 
 the profile is confirmed and the user is redirected to the main page.
 If the code is incorrect, the user is shown an error"""
 
@@ -45,9 +58,14 @@ def confirm_email(request):
             profile = Profile.objects.get(confirmation_code=code)
             profile.is_confirmed = True
             profile.save()
-            return redirect('accounts:login')
+
+            user = profile.user
+            login(request, user)
+
+            return redirect('autho_app:index')
+        
         except Profile.DoesNotExist:
-            error = "The code is incorrect. Check it again."
+            error = "The code is incorrect. Please check it again."
             return render(request, 'registration/confirm_email.html', {'error': error})
         
     return render(request, 'registration/confirm_email.html')
